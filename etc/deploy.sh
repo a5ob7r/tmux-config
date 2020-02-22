9d20e52d7c84279d7b2b695682e0a306bb6b6cf1
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2020-02-22 20:33:30 +0900

    Update a deploy script

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
+#
 # A deploy script to remote server
+#
 
 # Set useful shell options
 set -Cueo pipefail
 
-cd /tmp
-curl -L https://api.github.com/repos/a5ob7r/tmux-config/tarball | tar zx
-install -d ~/.config
-mv a5ob7r-tmux-config-* ~/.config/tmux
-cd ~/.config/tmux
-grep -Ev 'tmux-plugins/tmux-battery|@TMUX_CZ' tmux.conf >> ~/.tmux.conf
+readonly REPO_URL='https://api.github.com/repos/a5ob7r/tmux-config/tarball'
+readonly CONF_DIR=~/.config
+readonly TMUX_CONFIG_DIR="${CONF_DIR}/tmux"
+
+readonly TMP_DIR="$(mktemp -d /tmp/a5ob7r_tmux-config_XXXXXXXX)"
+trap 'rm -vrf ${TMP_DIR}' 0
+trap 'rm -vrf ${TMP_DIR}; exit 1' 1 2 3 15
+echo ":: Make a temporary working directory: ${TMP_DIR}"
+
+echo ":: Download a archived repository and extract it"
+curl -L "${REPO_URL}" | tar zx -C "${TMP_DIR}"
+readonly REPO_DIR=$(find "${TMP_DIR}" -mindepth 1 -maxdepth 1 -type d -name "a5ob7r-tmux-config-*")
+
+if [[ -d "${CONF_DIR}" ]]; then
+  echo ":: A config directory already exists: ${CONF_DIR}"
+else
+  echo ":: Make a config directory: ${CONF_DIR}"
+  mkdir -vp "${CONF_DIR}"
+fi
+
+echo ":: Move a repository directory to a tmux config directory"
+mv -vT --backup=t "${REPO_DIR}" "${TMUX_CONFIG_DIR}"
+
+echo ":: Generate ~/.tmux.conf wihch is remote version into your home directory"
+readonly TMPFILE="$(mktemp tmux.conf.XXXXXXXX)"
+grep -Ev 'tmux-plugins/tmux-battery|@TMUX_CZ' "${TMUX_CONFIG_DIR}/tmux.conf" >| "${TMPFILE}"
+mv -v --backup=t "${TMPFILE}" ~/.tmux.conf
